be5b20247a64328dc38a5c9c0387c64fdbea062d
danielcattlin <daniecattlin@hotmail.co.uk>
2015-10-11 15:26:34 +0100

    Adding voice engine func

---
+++ VoiceEngine.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Voice.h"
+
+class VoiceEngine
+{
+private:
+  static const int NumberOfVoices = 64;
+  Voice voices[NumberOfVoices];
+  Oscillator mLFO;
+  Voice* findFreeVoice();
+public:
+  VoiceEngine();
+  ~VoiceEngine();
+  void onNoteOn(int noteNumber, int velocity);
+  void onNoteOff(int noteNumber, int velocity);
+  double nextSample();
+};
+
