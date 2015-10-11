37b8a1302a09e953da61958cb7b28d4f4c3982c9
danielcattlin <daniecattlin@hotmail.co.uk>
2015-10-11 15:19:24 +0100

    Adding voice func

---
+++ Voice.cpp
@@ -0,0 +1,36 @@
+#include "Voice.h"
+
+Voice::~Voice() {}
+
+void Voice::reset() {
+  mNoteNumber = -1;
+  mVelocity = 0;
+  for (int i = 0; i < mActiveOscs; i++) {
+    mOsc[i].reset();
+  }
+  mVolumeEnvelope.reset();
+  mFilterEnvelope.reset();
+  mFilter.reset();
+}
+
+double Voice::nextSample() {
+  if (!isActive) return 0.0;
+
+  double oscSum = 0;
+  double oscAvr = 1 / mActiveOscs;
+
+  for (int i = 0; i < mActiveOscs; i++) {
+    oscSum += oscAvr * mOsc[i].nextSample() * mOscVol[i];
+  }
+
+  double volumeEnvelopeValue = mVolumeEnvelope.nextSample();
+  double filterEnvelopeValue = mFilterEnvelope.nextSample();
+
+  mFilter.setCutoffMod(filterEnvelopeValue * mFilterEnvelopeAmount + mLFOValue * mLFOAmount;
+
+  return mFilter.process(oscSum * volumeEnvelopeValue * mVelocity / 127.0);
+}
+
+void Voice::setFree() {
+  isActive = false;
+}
---
+++ Voice.h
@@ -0,0 +1,68 @@
+#pragma once
+#include "Oscillator.h"
+#include "EnvelopeGenerator.h"
+#include "Filter.h"
+
+#define MAX_OSC 3
+
+class Voice
+{
+private:
+  Oscillator mOsc[MAX_OSC];
+  EnvelopeGenerator mVolumeEnvelope, mFilterEnvelope;
+  Filter mFilter;
+  int mNoteNumber;
+  int mVelocity;
+  int mActiveOscs;
+  double mFilterEnvelopeAmount, mFilterCutoff, mFilterReso;
+  double mOscVol[MAX_OSC];
+  double mOscPitch[MAX_OSC];
+  double mOscDetune[MAX_OSC];
+  double mOscPhase[MAX_OSC];
+  double mOscPan[MAX_OSC];
+  double mLFOAmount, mLFOFreq, mLFOOffset, mLFOGain, mLFOValue;
+  bool isActive;
+public:
+  friend class VoiceEngine;
+  Voice()
+    : mNoteNumber(-1),
+    mVelocity(0),
+    mFilterEnvelopeAmount(0.0),
+    mFilterCutoff(0.0),
+    mFilterReso(0.0),
+    mLFOAmount(0.0),
+    mLFOFreq(0.0),
+    mLFOOffset(0.0),
+    mLFOGain(0.0),
+    mLFOValue(0.0),
+    mActiveOscs(1),
+    isActive(false) {
+      // Set myself free everytime my volume envelope has fully faded out of RELEASE stage:
+      mVolumeEnvelope.finishedEnvelopeCycle.Connect(this, &Voice::setFree);
+    };
+
+  inline void setFilterEnvelopeAmount(double amount) { mFilterEnvelopeAmount = amount; }
+  inline void setOscillatorPitch(int osc, double amount) { mOscPitch[osc] = amount; }
+  inline void setOscillatorDetune(int osc, double amount) { mOscDetune[osc] = amount; }
+  inline void setOscillatorVolume(int osc, double amount) { mOscVol[osc] = amount; }
+  inline void setOscillatorPhase(int osc, double amount) { mOscPhase[osc] = amount; }
+  inline void setOscillatorPan(int osc, double amount) { mOscPan[osc] = amount; }
+  inline void setOscillatorVol(int osc, double amount) { mOscPan[osc] = amount; }
+  inline void setLFOAmount(double amount) { mLFOAmount = amount; }
+  inline void setLFOOffset(double amount) { mLFOOffset = amount; }
+  inline void setLFOFreq(double amount) { mLFOFreq = amount; }
+  inline void setLFOGain(double amount) { mLFOGain = amount; }
+  inline void setLFOValue(double amount) { mLFOValue = amount; }
+  inline void setNoteNumber(int noteNumber) {
+    mNoteNumber = noteNumber;
+    double frequency = 440.0 * pow(2.0, (mNoteNumber - 69.0) / 12.0);
+    for (int i = 0; i < MAX_OSC; i++) {
+      mOsc[i].setFrequency(frequency);
+    }
+  }
+
+  void reset();
+  double nextSample();
+  void setFree();
+};
+
